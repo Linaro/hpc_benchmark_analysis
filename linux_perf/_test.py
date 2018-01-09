9a78721c42c59c06ad846c769fb19adbaf1844c4
Renato Golin <renato.golin@linaro.org>
2018-01-09 17:11:55 +0000

    Adding PerfData, parsing output, better test

--- linux_perf/_test.py
+++ linux_perf/_test.py
@@ -1,7 +1,28 @@
 #!/usr/bin/env python3
 
+"""Testing script for linux_perf functionality"""
+
 from linux_perf import LinuxPerf
 
-date = LinuxPerf(['date'])
-date.stat()
-date.parse()
+def _test_simple():
+    print("Test Simple: ", end='')
+    date = LinuxPerf(['date'])
+    date.stat()
+    date.parse()
+
+    inst = int(date.get_value('instructions'))
+    assert inst > 100000, 'Number of instructions too low'
+    assert inst < 1000000, 'Number of instructions too high'
+
+    branches = int(date.get_value('branches'))
+    assert branches > 100000, 'Number of branches too low'
+    assert branches < 1000000, 'Number of branches too high'
+
+    cpum = int(date.get_value('cpu-migrations'))
+    assert cpum >= 0, 'Number of instructions too low'
+    assert cpum < 10, 'Number of instructions too high'
+
+    print("PASS")
+
+# Tests
+_test_simple()
