9dcda3d5853504b32d06a7d2e112efd1d008ae4c
Renato Golin <renato.golin@linaro.org>
2018-01-18 14:06:59 +0000

    Adding "regression" tests for aggregate.py
    The test script basically runs it and checks for some consistency in the
    output (correct categories, number of results and that it parses both
    perf and Lulesh correctly).
    
    This is a horrible way of testing but will do for now. We should create
    a separate test directory and research some proper testing
    infrastructure for python.

--- test.sh
+++ test.sh
@@ -1,5 +1,45 @@
 #!/usr/bin/env bash
 
+# Horrible test driver, use something decent
+
+# "Unit" tests
+echo " * Unit Tests *"
 for dir in Lulesh linux_perf analysis; do
   python3 $dir/_test.py
 done
+
+# "Regression" tests
+echo
+echo " * Regression Tests *"
+
+out=$(python3 ./aggregate.py Lulesh x86_64)
+
+# Records
+logs=$(echo "$out" | grep -c Logfile)
+# Perf results
+inst=$(echo "$out" | grep -c instructions)
+# Lulesh results
+diff=$(echo "$out" | grep -c MaxRelDiff)
+# Should all be the same
+if [ "$logs" -ne "$inst" ] || [ "$inst" -ne "$diff" ]; then
+  echo "Results don't match: [$logs] [$inst] [$diff]"
+elif [ "$logs" -eq "0" ]; then
+  echo "Results are all zero?"
+else
+  echo "Results: PASS"
+fi
+
+# Categories
+cat1=$(echo "$out" | grep "^\\w" | sort -u | tr -d '[:space:]')
+cat2=$(echo "$out" | grep "^  \\w" | sort -u | tr -d '[:space:]')
+cat3=$(echo "$out" | grep "^    \\w" | sort -u | tr -d '[:space:]')
+if [ "$cat1" == "gcc6llvm4llvm5" ] &&
+   [ "$cat2" == "genericnative" ] &&
+   [ "$cat3" == "1248" ]; then
+  echo "Categories: PASS"
+else
+  echo "Categories incorrect:"
+  echo "Cat 1: [$cat1]"
+  echo "Cat 2: [$cat2]"
+  echo "Cat 3: [$cat3]"
+fi
