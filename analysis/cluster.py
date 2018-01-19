a295ddb52a73bebe9b26ef38790d2218cd4de9a6
Renato Golin <renato.golin@linaro.org>
2018-01-19 17:13:11 +0000

    Simple clustering of data
    Clustering is aimed at results that are known to be K-modal. The current
    model works well for data without large outliers, but behaves
    erradically when a distant outlier pull a centre all to itself and
    defies the purpose.
    
    This may call for an iterative approach, first finding global outliers,
    then removing them and clustering, then finding cluster outliers, etc.
    
    Also not implemented is any attempr to figure out the best number of
    clusters rather than force users to hard code. This can be done in a
    number of ways, including the pedestrian:
      for i in 1..k:
        best = min(best, kmeans(i).distances())
    all the way to using SciKit:
      http://scikit-learn.org/stable/modules/clustering.html

---
+++ analysis/cluster.py
@@ -0,0 +1,93 @@
+"""
+ Cluster Module - group 1D data in clusters using K-Means
+
+ Usage:
+   clus = Clustering([...data...])
+   clus.kmeans(3)
+   print(repr(clus.get_outliers())
+
+ Ideas:
+  - http://scikit-learn.org/stable/modules/clustering.html
+"""
+
+import numpy as np
+from outlier import Outliers
+
+class Cluster:
+    """Data class with a specific cluster"""
+    def __init__(self, centre):
+        self.data = list()
+        self.centre = float(centre)
+
+    def set_points(self, points):
+        """Override data, returns true if centre changed"""
+        if not isinstance(points, list):
+            raise "Cluster points must be of array type"
+        self.data = points
+        changed = False
+        # Cluster can have no points
+        if points:
+            mean = np.mean(self.data)
+        else:
+            mean = float('nan')
+        # If changed, return true
+        if mean != self.centre:
+            changed = True
+        self.centre = mean
+        return changed
+
+    def get_outliers(self):
+        """Uses Outlier module to find outliers, if any"""
+        out = Outliers(self.data)
+        return out.get_outliers()
+
+class Clustering:
+    """Utility class to calculate clustering in data sets"""
+    def __init__(self, data, max_iter=10):
+        self.data = np.array(data)
+        self.clusters = list()
+        self.max_iter = max_iter
+        self.done = False
+
+    def kmeans(self, num_clusters=1):
+        """Find K clusters"""
+        # Create K random centres
+        for cent in range(num_clusters):
+            self.clusters.append(Cluster(np.random.randint(np.min(self.data),
+                                                           np.max(self.data))))
+        # List of points and centres they belong to
+        belongs = np.zeros(len(self.data), np.int)
+
+        # While centres move around (or up to max_iter)
+        for _ in range(self.max_iter):
+            changed = False
+
+            # Assign points to closest centre
+            for pnt in range(len(self.data)):
+                dist = [abs(self.data[pnt]-self.clusters[i].centre)
+                        for i in range(num_clusters)]
+                cluster = np.argmin(dist)
+                belongs[pnt] = cluster
+
+            # Add all points to each centre and move to its mean
+            for cent in range(num_clusters):
+                new_points = [self.data[j]
+                              for j in range(len(self.data))
+                              if belongs[j] == cent]
+                changed |= self.clusters[cent].set_points(new_points)
+
+            # If nothing changed, it's solved
+            if not changed:
+                break
+
+        self.done = True
+        return self.clusters
+
+    def get_outliers(self):
+        """Get all outliers of all clusters"""
+        if not self.done:
+            self.kmeans()
+        outliers = list()
+        for cluster in self.clusters:
+            outliers.extend(cluster.get_outliers())
+        return outliers
